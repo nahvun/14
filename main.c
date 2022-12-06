db261eabf1b521c2265b9a370452c3a6d5c55c7b
nahvun <nahyun@sookmyung.ac.kr>
2022-12-06 16:45:44 +0900

    14_03

--- main.c
+++ main.c
@@ -3,31 +3,35 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-struct Book {
+struct Book
+{
 	int number;
 	char title[10];
 };
 
-int main(void)
+void main(void)
 {
-	struct Book *p;
+	int i;
+	struct Book **bookshelf;
 	
-	p = (struct Book*)malloc(2*sizeof(struct Book));
+	bookshelf = (struct Book**)malloc(3*sizeof(struct Book*));
+	for(i=0;i<3;i++)
+		bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book));
+		
+	bookshelf[1][3].number = 5;
+	strcpy(bookshelf[1][3].title, "C++ Programming");
 	
-	if (p == NULL)
-	{
-		printf("메모리 할당 오류\n");
-		return -1;
-	}
+	(bookshelf[2]+4)->number = 3;
+	strcpy((bookshelf[2]+4)->title, "Communications Theory");
 	
-	p->number = 1;
-	strcpy(p->title , "C Programming");
+	printf("book (1, 3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+	printf("book (2, 4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+
+	for(i=0;i<3;i++)
+		free(bookshelf[i]);
+	free(bookshelf);
 	
-	(p+1)->number = 2;
-	strcpy((p+1)->title , "Electronics");
-	
-	free(p);
-	return 0;
+	return;
+
 }
 
